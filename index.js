44a42f7ee45f03b9b3b3459fad8cb677bb1cd63f
iamrivera <a3m3rivera@gmail.com>
2021-07-10 18:32:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books').then(function(response) {
+    return response.json();
+}).then(function(json) {
+    renderBooks(json);
+});
 }
 
 function renderBooks(books) {
@@ -14,4 +18,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
